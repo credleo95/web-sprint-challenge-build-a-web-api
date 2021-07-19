b4ef40b36af55ca8c1ce0b79e9e51ea909e17bce
Omarius <70237021+credleo95@users.noreply.github.com>
2021-07-18 23:45:30 -0400

    server configured. First route completed.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,49 @@
 // Write your "projects" router here!
+
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+
+const router = express.Router();
+/*- [ ] `[GET] /api/projects`
+  - Returns an array of projects as the body of the response.
+  - If there are no projects it responds with an empty array.*/
+router.get('/', (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      if (projects.length === 0) {
+        res.send([]);
+      }
+      res.send(projects);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+/*
+- [ ] `[GET] /api/projects/:id`
+  - Returns a project with the given `id` as the body of the response.
+  - If there is no project with the given `id` it responds with a status code 404.
+- [ ] `[POST] /api/projects`
+  - Returns the newly created project as the body of the response.
+  - If the request body is missing any of the required fields it responds with a status code 400.
+- [ ] `[PUT] /api/projects/:id`
+  - Returns the updated project as the body of the response.
+  - If there is no project with the given `id` it responds with a status code 404.
+  - If the request body is missing any of the required fields it responds with a status code 400.
+- [ ] `[DELETE] /api/projects/:id`
+  - Returns no response body.
+  - If there is no project with the given `id` it responds with a status code 404.
+- [ ] `[GET] /api/projects/:id/actions`
+  - Returns an array of actions (could be empty) belonging to a project with the given `id`.
+  - If there is no project with the given `id` it responds with a status code 404.
+
+
+  ***HELPERS***
+
+  - `get()`: resolves to an array of all the resources contained in the database. If you pass an `id` to this method it will return the resource with that id if one is found.
+- `insert()`: calling insert passing it a resource object will add it to the database and return the newly created resource.
+- `update()`: accepts two arguments, the first is the `id` of the resource to update, and the second is an object with the `changes` to apply. It returns the updated resource. If a resource with the provided `id` is not found, the method returns `null`.
+- `remove()`: the remove method accepts an `id` as its first parameter and, upon successfully deleting the resource from the database, returns the number of records deleted.*/
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const projectsRouter = require('./projects/projects-router.js');
 const server = express();
+server.use(express.json());
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use('/api/projects', projectsRouter);
 
 module.exports = server;
